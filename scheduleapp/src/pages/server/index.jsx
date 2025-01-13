a7982105d5992e045f36f4df96966a18a48c4b9c
Kaio <kaio.santiago.13@hotmail.com>
2025-01-12 22:01:58 -0300

    added div to edit server

--- scheduleapp/src/pages/server/index.jsx
+++ scheduleapp/src/pages/server/index.jsx
@@ -1,41 +1,81 @@
-import React from "react";
-import './styles.css'
+import React, { useState } from "react";
+import './styles.css';
 import Layout from '../layoutCommon/'; 
 import { FaTrash, FaEdit } from 'react-icons/fa';
 
-export default function Server(){
+export default function Server() {
+    const [isEditing, setIsEditing] = useState(false); // Estado para controlar a visibilidade da div de edição
+    const [editData, setEditData] = useState({ tagName: '', ip: '' }); // Estado para armazenar os dados do servidor que serão editados
 
+    // Dados dos servidores
     const dados = [
-        { edit: <FaEdit/>, delete: <FaTrash />, tagName: 'Server_1', ip: "192.168.1.1" },
-        { edit: <FaEdit/>, delete: <FaTrash />, tagName: 'Server_2', ip: "192.168.1.2" },
-        { edit: <FaEdit/>, delete: <FaTrash />, tagName: 'Server_3', ip: "192.168.1.4" },
-      ];
+        { id: 1, edit: <FaEdit onClick={() => handleEdit('Server_1', '192.168.1.1')}/>, delete: <FaTrash />, tagName: 'Server_1', ip: "192.168.1.1" },
+        { id: 2, edit: <FaEdit onClick={() => handleEdit('Server_2', '192.168.1.2')}/>, delete: <FaTrash />, tagName: 'Server_2', ip: "192.168.1.2" },
+        { id: 3, edit: <FaEdit onClick={() => handleEdit('Server_3', '192.168.1.4')}/>, delete: <FaTrash />, tagName: 'Server_3', ip: "192.168.1.4" },
+    ];
+
+    const handleEdit = (tagName, ip) => {
+        setEditData({ tagName, ip }); 
+        setIsEditing(true); 
+    };
+
+    const closeEdit = () => {
+        setIsEditing(false);
+    };
 
     return (
         <Layout>
             <div className="home-content">
                 <div className="container-table">
-                <table border="1">
-                    <thead>
-                        <tr>
-                        <th>Edit</th>
-                        <th>Delete</th>
-                        <th>TagName</th>
-                        <th>IP Address</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {dados.map((dado) => (
-                        <tr key={dado.id}>
-                            <td>{dado.edit}</td>
-                            <td>{dado.delete}</td>
-                            <td>{dado.tagName}</td>
-                            <td>{dado.ip}</td>
-                        </tr>
-                        ))}
-                    </tbody>
-                </table>
+                    <table border="1">
+                        <thead>
+                            <tr>
+                                <th>Edit</th>
+                                <th>Delete</th>
+                                <th>TagName</th>
+                                <th>IP Address</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {dados.map((dado) => (
+                                <tr key={dado.id}>
+                                    <td>{dado.edit}</td>
+                                    <td>{dado.delete}</td>
+                                    <td>{dado.tagName}</td>
+                                    <td>{dado.ip}</td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
                 </div>
+
+                {/* Div de Edição, visível quando isEditing for true */}
+                {isEditing && (
+                    <div className="edit-container">
+                        <div className="div-edit">
+                            <label className="lb-server">
+                                TagName:
+                                <input className="input-server"
+                                    type="text"
+                                    value={editData.tagName}
+                                    onChange={(e) => setEditData({ ...editData, tagName: e.target.value })}
+                                />
+                            </label>
+                            <label className="lb-ip">
+                                IP Address:
+                                <input className="input-ip"
+                                    type="text"
+                                    value={editData.ip}
+                                    onChange={(e) => setEditData({ ...editData, ip: e.target.value })}
+                                />
+                        </label>
+                        </div>
+                        <div className="div-buttons">
+                            <button className="btn-close" onClick={closeEdit}>Close</button>
+                            <button className="btn-save" onClick={() => console.log('Save changes:', editData)}>Save</button>
+                        </div>
+                    </div>
+                )}
             </div>
         </Layout>
     );
